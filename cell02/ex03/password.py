df095866e3b23f8c23bcdd3f9afd345499d24ab2
ttongnge <66011259@kmitl.ac.th>
2025-06-25 13:26:29 +0000

    cell02 ex03

---
+++ cell02/ex03/password.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+password = "python is awesome"
+
+enter_password = input()
+
+if enter_password == password:
+    print("access granted")
+else:
+    print("access denied")
+
+    
+
+
