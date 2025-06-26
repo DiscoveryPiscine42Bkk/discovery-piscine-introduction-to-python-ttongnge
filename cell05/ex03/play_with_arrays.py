aef783fbe42c46bbb559f4afab6acdf4a2d3e791
ttongnge <66011259@kmitl.ac.th>
2025-06-26 21:26:01 +0000

    5/05

---
+++ cell05/ex03/play_with_arrays.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+    new_array = {x + 2 for x in original_array if x > 5}  # Using set comprehension
+    
+    print(original_array, end='$\n')
+    print(new_array, end='$\n')
+
+if __name__ == "__main__":
+    main()
